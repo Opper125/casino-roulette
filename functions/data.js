10f1cbbd07a3f0fbe030ff1abefd641a443572eb
Opper125 <crakb68@gmail.com>
2025-03-04 01:45:40 +0630

    Update data.js

--- functions/data.js
+++ functions/data.js
@@ -1,37 +1,24 @@
-const fs = require('fs');
-const path = require('path');
-
-const DATA_FILE = path.join(__dirname, 'casino-data.json');
-
-function readData() {
-  if (!fs.existsSync(DATA_FILE)) {
-    const defaultData = { users: {}, requests: {}, messages: [], game: { state: 'betting', timeLeft: 30, onlinePlayers: 0, result: null, adminStarted: false, history: [] }, transactions: {} };
-    fs.writeFileSync(DATA_FILE, JSON.stringify(defaultData));
-    return defaultData;
-  }
-  return JSON.parse(fs.readFileSync(DATA_FILE, 'utf8'));
-}
-
-function writeData(data) {
-  fs.writeFileSync(DATA_FILE, JSON.stringify(data));
-}
+let casinoData = {
+  users: {},
+  requests: {},
+  messages: [],
+  game: { state: 'betting', timeLeft: 30, onlinePlayers: 0, result: null, adminStarted: false, history: [] },
+  transactions: {}
+};
 
 exports.handler = async (event, context) => {
   try {
     if (event.httpMethod === 'GET') {
-      const data = readData();
       return {
         statusCode: 200,
-        body: JSON.stringify(data),
+        body: JSON.stringify(casinoData),
       };
     } else if (event.httpMethod === 'POST') {
       const updates = JSON.parse(event.body);
-      const data = readData();
-      const newData = { ...data, ...updates };
-      writeData(newData);
+      casinoData = { ...casinoData, ...updates };
       return {
         statusCode: 200,
-        body: JSON.stringify(newData),
+        body: JSON.stringify(casinoData),
       };
     } else {
       return {
